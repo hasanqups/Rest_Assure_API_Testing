3e3a85d16710060ee81b096a95a84da132a108be
Tarif <abir.tariful.islam@gmail.com>
2022-08-20 21:24:19 +0600

    Updated Jenkins

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -6,13 +6,13 @@ import org.testng.annotations.DataProvider;
 
 @CucumberOptions(features = "src/test/java/features",
         glue = "StepDefinations",
-        tags = "@Get",
+//        tags = "@Get",
         plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json"}
 )
 
 public class TestRunner extends AbstractTestNGCucumberTests {
     @Override
-    @DataProvider(parallel = true)
+    @DataProvider(parallel = false)
     public Object[][] scenarios() {
         return super.scenarios();
     }
