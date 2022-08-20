2e5854578f0558ebf5affb652283b129611c270e
Tarif <abir.tariful.islam@gmail.com>
2022-08-20 20:21:26 +0600

    Jenkins Integration

---
+++ src/test/java/utils/ResourceHelper.java
@@ -0,0 +1,25 @@
+package utils;
+
+import java.util.ResourceBundle;
+
+public class ResourceHelper {
+    public ResourceBundle bundle;
+
+    public ResourceHelper getResource(String file) {
+        bundle = ResourceBundle.getBundle(file);
+        return this;
+    }
+
+    public String getString(String key) {
+        return System.getProperty(key) == null ? bundle.getString(key) : System.getProperty(key);
+    }
+
+    public boolean getBool(String key) {
+        return Boolean.parseBoolean(getString(key));
+    }
+
+    public int getInt(String key) {
+        return Integer.parseInt(getString(key));
+    }
+
+}
